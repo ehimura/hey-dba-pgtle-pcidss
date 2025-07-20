3087654db63e89c4b6f560b36ad1028e0543079e
Evandro Giachetto <evandrogiachetto@gmail.com>
2025-07-20 23:54:15 +0100

    Add Copyright Notice

--- src/pci_password_check_rules_0.5_up_0.4-0.5.sql
+++ src/pci_password_check_rules_0.5_up_0.4-0.5.sql
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025 Evandro Giachetto <evandro@hey-dba.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+ 
 --0.5 changes:
 -- Version 0.5 Update Path (Part 7: The Bouncer at the Gate – Implementing Account Lockout and Inactive Account Management)
 -- This script updates the 'pci_password_check_rules' pg_tle extension from version 0.4 to 0.5.
